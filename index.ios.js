bc86bfa19ff927ed9b8401ee3d3b833ff5e5c378
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-27 11:28:15 -0700

    Initial Commit

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactNative from 'react-native';
+
+const App = () => {
+  return (
+    <Text> Hello! </Text>
+  );
+};
+
+ReactNative.AppRegistry.registerComponent('albums', () => App )
