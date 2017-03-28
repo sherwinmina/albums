0824a9c6903e86ef7804ef1a3a92840e9f7d1b60
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-27 17:24:30 -0700

    added onPress Method to Button

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -36,7 +36,7 @@ const AlbumDetail = ({ album }) => {
         />
       </CardSection>
       <CardSection>
-        <Button />
+        <Button onPress={() => console.log(title)} />
       </CardSection>
     </Card>
   );
